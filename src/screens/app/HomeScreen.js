a803e7ee386db85ea624c0c4483ff85a01079a6c
julfinch <julfinch@gmail.com>
2023-06-17 23:48:16 +0800

    update home page

--- src/screens/app/HomeScreen.js
+++ src/screens/app/HomeScreen.js
@@ -1,23 +1,30 @@
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Image, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 const HomeScreen = ({navigation}) => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#212244', }}>
       <ScrollView style={{padding: 20}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            marginBottom: 20,
-          }}>
-          <Text style={{fontSize: 18}}>
-            Hello John Doe
-          </Text>
-          <TouchableOpacity onPress={() => navigation.openDrawer()}>
-            <View style={{ height: 100, width: 200, backgroundColor: 'red'}}></View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 20, }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'center', alignItem: 'center', gap: 10, }}>
+            <Image
+              source={require('../../assets/doodles-pic.png')}
+              style={{height: 40, width: 40, borderRadius: 50,borderColor:'#fff',borderWidth: 1, marginBottom: 10}}
+            />
+            <View>
+              <Text style={{color: 'rgba(255,255,255,0.6)'}}>Welcome back</Text>
+              <Text style={{color: '#fff', fontWeight: '700',}}>Lara Turner</Text>
+            </View>
+          </View>
+          <TouchableOpacity onPress={() => navigation.openDrawer()} style={{width: '10%', }}>
+            <Ionicons name="menu-outline" color="#fff" size={33} style={{textAlign: 'right'}}/>
           </TouchableOpacity>
         </View>
+        <Text style={{fontSize: 18}}>
+            Hello John Doe
+          </Text>
       </ScrollView>
     </SafeAreaView>
   )
